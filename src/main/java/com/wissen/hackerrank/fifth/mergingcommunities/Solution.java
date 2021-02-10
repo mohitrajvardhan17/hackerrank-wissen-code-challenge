e0e2b64bdd2fafdc340fce2d1bb8a8912044f116
Mohit Rajvardhan <mohitrajvardhan17@gmail.com>
2021-02-10 22:32:42 +0530

    Final efficient approach for merging communities

--- src/main/java/com/wissen/hackerrank/fifth/mergingcommunities/Solution.java
+++ src/main/java/com/wissen/hackerrank/fifth/mergingcommunities/Solution.java
@@ -1,11 +1,6 @@
 package com.wissen.hackerrank.fifth.mergingcommunities;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Scanner;
 
 /*
 ##############NOTES##############Todo: Complete this.
@@ -53,38 +48,109 @@ Explanation :
 Initial size of each of the community is 1.
  */
 public class Solution {
+    /**
+     * STEP 1: Define a Person class
+     * This class provides implementation for storage of a parent node of a person as parent and number of child
+     * node as size.
+     */
+    static class Person {
+        int parent = -1;
+        int size = 1;
+    }
 
-    public static void main(String[] args) throws IOException {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input[] = br.readLine().split(" ");
-        int people = Integer.parseInt(input[0]);
-        int queries = Integer.parseInt(input[1]);
-        int[] group = new int[people];
-        for (int i = 0; i < group.length; i++) {
-            group[i] = 1;
+    /**
+     * STEP 2: Define a CommunityQueryProcessor class
+     * This class provides implementation for processing community query such as the merge query ond the size query.
+     */
+    static class CommunityQueryProcessor {
+        private final Person[] people;
+
+        /**
+         * STEP 3: Define a CommunityQueryProcessor class constructor
+         * This constructor creates the memory with required number of person element in the people array.
+         */
+        public CommunityQueryProcessor(int numberOfPeople) {
+            people = new Person[numberOfPeople + 1];
         }
-        Map<Integer, Integer> connection = IntStream.range(0, group.length)
-                .boxed()
-                .collect(Collectors.toMap(k -> (Integer) k, v -> (Integer) v));
-        for (int i = 0; i < queries; i++) {
-            input = br.readLine().split(" ");
-            if ("M".equalsIgnoreCase(input[0].trim())) {
-                int firstPerson = Integer.parseInt(input[1]) - 1;
-                int secondPerson = Integer.parseInt(input[2]) - 1;
-                int firstGroupIndex = connection.get(firstPerson);
-                int secondGroupIndex = connection.get(secondPerson);
-                if(firstGroupIndex != secondGroupIndex &&
-                        (group[firstGroupIndex] != -1 || group[secondGroupIndex] != -1)) {
-                    connection.entrySet().stream().filter(e -> e.getValue() == secondGroupIndex).forEach(d -> {
-                        connection.put(d.getKey(), firstGroupIndex);
-                    });
-                    group[firstGroupIndex] = group[firstGroupIndex] + group[secondGroupIndex];
-                    group[secondGroupIndex] = 0;
-                }
-            } else if ("Q".equalsIgnoreCase(input[0].trim())) {
-                int index = Integer.parseInt(input[1]) - 1;
-                System.out.println(group[connection.get(index)]);
+
+        /**
+         * STEP 4: Define a initializeCommunity() method in CommunityQueryProcessor class
+         * This method initializes all the person element of people array with new person objects.
+         */
+        public void initializeCommunity(int numberOfPeople) {
+            for (int i = 1; i <= numberOfPeople; i++) {
+                people[i] = new Person();
+            }
+        }
+
+        /**
+         * STEP 5: Define a processCommunitySizeQueryByPersonIndex() method in CommunityQueryProcessor class
+         * This method iterate over the people array and find out the root person index and also flatten the the tree
+         * hierarchy.
+         * @param personIndex the person index for which root person index is required.
+         * @return the index of the root person.
+         */
+        private int findRootAndAdjustDepth(int personIndex) {
+            //Step 1 : Finding the parent for given person.
+            int root = personIndex;
+            while (people[root].parent != -1) {
+                root = people[root].parent;
+            }
+
+            //Step 2 : Adjust all the parent of parent as the root person so the depth decrease and search is efficient.
+            int person = personIndex;
+            while (person != root) {
+                int nextParent = people[person].parent;
+                people[person].parent = root;
+                person = nextParent;
+            }
+            return root;
+        }
+
+        /**
+         * STEP 6: Define a processCommunitySizeQueryByPersonIndex() method in CommunityQueryProcessor class
+         * This method iterate over the people array and find out the size of the community.
+         * @param personIndex the person index for which community size is required.
+         * @return the size of community of the person.
+         */
+        public int processCommunitySizeQueryByPersonIndex(int personIndex) {
+            return people[findRootAndAdjustDepth(personIndex)].size;
+        }
+
+        /**
+         * STEP 7: Define a processCommunityMergeQuery() method in CommunityQueryProcessor class
+         * This method iterate over the people array and find out the community of two person if the communities is
+         * not same then the communities are merged.
+         * @param firstPersonIndex the first person index for which community needs to be merged.
+         * @param secondPersonIndex the second person index for which community needs to be merged.
+         */
+        public void processCommunityMergeQuery(int firstPersonIndex, int secondPersonIndex) {
+            int firstPersonRoot = findRootAndAdjustDepth(firstPersonIndex);
+            int secondPersonRoot = findRootAndAdjustDepth(secondPersonIndex);
+            if (firstPersonRoot != secondPersonRoot) {
+                people[secondPersonRoot].parent = firstPersonRoot;
+                people[firstPersonRoot].size += people[secondPersonRoot].size;
+            }
+        }
+    }
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int people = SCANNER.nextInt();
+        int queries = SCANNER.nextInt();
+        CommunityQueryProcessor communityQueryProcessor = new CommunityQueryProcessor(people);
+        communityQueryProcessor.initializeCommunity(people);
+        for (int i = 1; i <= queries; i++) {
+            String command = SCANNER.next();
+            if ("M".equalsIgnoreCase(command)) {
+                int firstPersonIndex = SCANNER.nextInt();
+                int secondPersonIndex = SCANNER.nextInt();
+                communityQueryProcessor.processCommunityMergeQuery(firstPersonIndex, secondPersonIndex);
+            } else if ("Q".equalsIgnoreCase(command)) {
+                int personIndex = SCANNER.nextInt();
+                int result = communityQueryProcessor.processCommunitySizeQueryByPersonIndex(personIndex);
+                System.out.println(result);
             }
         }
     }
