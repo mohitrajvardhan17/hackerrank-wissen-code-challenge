ea2afcb51162945ccc716c77d69a78a7a3f9357d
Mohit Rajvardhan <mohitrajvardhan17@gmail.com>
2021-02-24 18:28:58 +0530

    Final efficient approach with disjoint set for merging communities

--- src/main/java/com/wissen/hackerrank/fifth/mergingcommunities/Solution.java
+++ src/main/java/com/wissen/hackerrank/fifth/mergingcommunities/Solution.java
@@ -1,10 +1,14 @@
 package com.wissen.hackerrank.fifth.mergingcommunities;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 
 /*
 ##############NOTES##############Todo: Complete this.
 ##############STEPS##############Todo: Complete this.
+1. Rank of a parent node increases only when both the node parent have same rank.
 ##############QUESTION##############
 https://www.hackerrank.com/contests/wissen-coding-challenge-2021/challenges/merging-communities
 People connect with each other in a social network. A connection between Person I and Person J is represented as MIJ.
@@ -48,95 +52,168 @@ Explanation :
 Initial size of each of the community is 1.
  */
 public class Solution {
-    /**
-     * STEP 1: Define a Person class
-     * This class provides implementation for storage of a parent node of a person as parent and number of child
-     * node as size.
-     */
-    static class Person {
-        int parent = -1;
-        int size = 1;
-    }
 
     /**
-     * STEP 2: Define a CommunityQueryProcessor class
-     * This class provides implementation for processing community query such as the merge query ond the size query.
+     * STEP 1: Define a DisjointSet generic class
+     * This class provides implementation of disjoint set data structure.
      */
-    static class CommunityQueryProcessor {
-        private final Person[] people;
+    class DisjointSet<T> {
+
+        private Map<T, Node<T>> nodeByData = new HashMap<>();
 
         /**
-         * STEP 3: Define a CommunityQueryProcessor class constructor
+         * STEP 2: Define a Node class
+         * This class provides implementation for storage of node in disjoint set data structure.
+         */
+        public class Node<T> {
+            final T data;
+            int rank;
+            int size;
+            Node parent = null;
+
+            public Node(T data) {
+                this.data = data;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Node node = (Node) o;
+                return rank == node.rank && parent == node.parent && data.equals(node.data);
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(data, rank, parent);
+            }
+        }
+
+        /**
+         * STEP 3: Define a getNode() method in DisjointSet class
+         * This method takes data and returns the associated node in the disjoint set.
+         *
+         * @param data the data for which node is required.
+         * @return The node for the given data or null.
+         */
+        public Node<T> getNode(T data) {
+            return nodeByData.get(data);
+        }
+
+        /**
+         * STEP 4: Define a makeSet() method in DisjointSet class
+         * This method takes one record and adds to the disjoint set.
+         *
+         * @param data the record which needs to be inserted in the disjoint set.
+         */
+        public void makeSet(final T data) {
+            Node<T> newNode = new Node<T>(data);
+            newNode.parent = newNode;
+            newNode.rank = 0;
+            newNode.size = 1;
+            nodeByData.put(data, newNode);
+        }
+
+        /**
+         * STEP 5: Define a union() method in DisjointSet class
+         * This method takes two record and finds out the group of both the record if both records are from
+         * different then both are the groups are merged into one group.
+         *
+         * @param firstData  the first record for which group needs to be found and merged.
+         * @param secondData the second record for which group needs to be found and merged.
+         */
+        public void union(final T firstData, final T secondData) {
+            Node<T> firstNode = nodeByData.get(firstData);
+            Node<T> secondNode = nodeByData.get(secondData);
+            Node<T> firstNodeParent = findSet(firstNode);
+            Node<T> secondNodeParent = findSet(secondNode);
+
+            if (firstNodeParent.data == secondNodeParent.data) {
+                return;
+            }
+
+            if (firstNodeParent.rank >= secondNodeParent.rank) {
+                firstNodeParent.size += secondNodeParent.size;
+                firstNodeParent.rank = (firstNodeParent.rank == secondNodeParent.rank) ?
+                        firstNodeParent.rank + 1 : firstNodeParent.rank;
+                secondNodeParent.parent = firstNodeParent;
+            } else {
+                secondNodeParent.size += firstNodeParent.size;
+                firstNodeParent.parent = secondNodeParent;
+            }
+        }
+
+        /**
+         * STEP 6: Define a findSet() method in DisjointSet class
+         * This method takes a node as input and finds the root node for the same input node.
+         *
+         * @param firstNode the node for which root node needs to be found.
+         * @return The parent node for the given node.
+         */
+        public Node<T> findSet(final Node firstNode) {
+            Node<T> parent = firstNode.parent;
+            if (parent == firstNode) {
+                return parent;
+            }
+            firstNode.parent = findSet(firstNode.parent);
+            return firstNode.parent;
+        }
+    }
+
+
+    /**
+     * STEP 7: Define a CommunityQueryProcessor class
+     * This class provides implementation for processing community query such as the merge query ond the size query.
+     */
+    class CommunityQueryProcessor {
+
+        private final DisjointSet<Integer> disjointSet;
+
+        /**
+         * STEP 8: Define a CommunityQueryProcessor class constructor
          * This constructor creates the memory with required number of person element in the people array.
          */
         public CommunityQueryProcessor(int numberOfPeople) {
-            people = new Person[numberOfPeople + 1];
+            this.disjointSet = new DisjointSet<Integer>();
+            initializeCommunity(numberOfPeople);
         }
 
         /**
-         * STEP 4: Define a initializeCommunity() method in CommunityQueryProcessor class
+         * STEP 9: Define a initializeCommunity() method in CommunityQueryProcessor class
          * This method initializes all the person element of people array with new person objects.
          */
-        public void initializeCommunity(int numberOfPeople) {
+        private void initializeCommunity(int numberOfPeople) {
             for (int i = 1; i <= numberOfPeople; i++) {
-                people[i] = new Person();
+                disjointSet.makeSet(i);
             }
         }
 
-        /**
-         * STEP 5: Define a processCommunitySizeQueryByPersonIndex() method in CommunityQueryProcessor class
-         * This method iterate over the people array and find out the root person index and also flatten the the tree
-         * hierarchy.
-         * @param personIndex the person index for which root person index is required.
-         * @return the index of the root person.
-         */
-        private int findRootAndAdjustDepth(int personIndex) {
-            //Step 1 : Finding the parent for given person.
-            int root = personIndex;
-            while (people[root].parent != -1) {
-                root = people[root].parent;
-            }
-
-            //Step 2 : Adjust all the parent of parent as the root person so the depth decrease and search is efficient.
-            int person = personIndex;
-            while (person != root) {
-                int nextParent = people[person].parent;
-                people[person].parent = root;
-                person = nextParent;
-            }
-            return root;
-        }
 
         /**
-         * STEP 6: Define a processCommunitySizeQueryByPersonIndex() method in CommunityQueryProcessor class
+         * STEP 10: Define a processCommunitySizeQueryByPersonIndex() method in CommunityQueryProcessor class
          * This method iterate over the people array and find out the size of the community.
+         *
          * @param personIndex the person index for which community size is required.
          * @return the size of community of the person.
          */
         public int processCommunitySizeQueryByPersonIndex(int personIndex) {
-            return people[findRootAndAdjustDepth(personIndex)].size;
+            return disjointSet.findSet(disjointSet.getNode(personIndex)).size;
         }
 
         /**
-         * STEP 7: Define a processCommunityMergeQuery() method in CommunityQueryProcessor class
+         * STEP 11: Define a processCommunityMergeQuery() method in CommunityQueryProcessor class
          * This method iterate over the people array and find out the community of two person if the communities is
          * not same then the communities are merged.
-         * @param firstPersonIndex the first person index for which community needs to be merged.
+         *
+         * @param firstPersonIndex  the first person index for which community needs to be merged.
          * @param secondPersonIndex the second person index for which community needs to be merged.
          */
         public void processCommunityMergeQuery(int firstPersonIndex, int secondPersonIndex) {
-            int firstPersonRoot = findRootAndAdjustDepth(firstPersonIndex);
-            int secondPersonRoot = findRootAndAdjustDepth(secondPersonIndex);
-            if (firstPersonRoot != secondPersonRoot) {
-                people[secondPersonRoot].parent = firstPersonRoot;
-                people[firstPersonRoot].size += people[secondPersonRoot].size;
-            }
+            disjointSet.union(firstPersonIndex, secondPersonIndex);
         }
     }
 
-    private static final Scanner SCANNER = new Scanner(System.in);
-
-    public static void main(String[] args) {
+    public void init(Scanner SCANNER) {
         int people = SCANNER.nextInt();
         int queries = SCANNER.nextInt();
         CommunityQueryProcessor communityQueryProcessor = new CommunityQueryProcessor(people);
@@ -153,5 +230,11 @@ public class Solution {
                 System.out.println(result);
             }
         }
+    }
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        new Solution().init(SCANNER);
     }
 }
